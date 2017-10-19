78634fa298c333a959048a0c13f70179d96be88e
Jeff Becker <jeff@i2p.rocks>
2017-10-19 14:14:49 -0400

    frontend filter bad media types

--- src/waveguide/cmd/frontend/upload.go
+++ src/waveguide/cmd/frontend/upload.go
@@ -1,40 +1,56 @@
 package frontend
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"mime"
 	"net/http"
 	"net/url"
 	"path/filepath"
+	"strings"
 	"time"
 	"waveguide/lib/model"
 	"waveguide/lib/util"
 )
+
+var ErrBadMediaType = errors.New("bad media type")
+var ErrBadContentType = errors.New("bad content type")
 
 func (r *Routes) HandleUpload(c *gin.Context) {
 	var videoURL string
 	u := r.GetCurrentUser(c)
 	video, err := c.FormFile("video")
 	if err == nil {
-		var vidID int64
-		vidID, err = r.DB.NextVideoID()
-		if err == nil {
+		ctype := mime.TypeByExtension(filepath.Ext(video.Filename))
+		if ctype != "" {
+			mtype, _, _ := mime.ParseMediaType(ctype)
+			if strings.HasPrefix(mtype, "video/") {
+				var vidID int64
+				vidID, err = r.DB.NextVideoID()
+				if err == nil {
 
-			info := &model.VideoInfo{
-				UserID:     u.UserID,
-				VideoID:    vidID,
-				Title:      video.Filename,
-				UploadedAt: time.Now().Unix(),
+					info := &model.VideoInfo{
+						UserID:     u.UserID,
+						VideoID:    vidID,
+						Title:      video.Filename,
+						UploadedAt: time.Now().Unix(),
+					}
+					ext := filepath.Ext(video.Filename)
+					tmpFile := util.TempFileName(r.TempDir, ext)
+					c.SaveUploadedFile(video, tmpFile)
+					fileURL := &url.URL{
+						Scheme: "file",
+						Path:   tmpFile,
+					}
+					if err == nil {
+						err = r.api.Do(info.VideoUploadRequest(fileURL, video.Filename))
+					}
+				}
+			} else {
+				err = ErrBadMediaType
 			}
-			ext := filepath.Ext(video.Filename)
-			tmpFile := util.TempFileName(r.TempDir, ext)
-			c.SaveUploadedFile(video, tmpFile)
-			fileURL := &url.URL{
-				Scheme: "file",
-				Path:   tmpFile,
-			}
-			if err == nil {
-				err = r.api.Do(info.VideoUploadRequest(fileURL, video.Filename))
-			}
+		} else {
+			err = ErrBadContentType
 		}
 	}
 	if err == nil {
