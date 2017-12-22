19c07cf7610cc59d3d08e7258b13e88c3f335463
Jeff Becker <jeff@i2p.rocks>
2017-12-22 16:02:03 -0500

    more

--- js/settings.js
+++ js/settings.js
@@ -6,11 +6,13 @@ const RefreshInterval = SegmentLength / 4;
 const SegMime = "video/webm;codecs=vp8";
 const SegExt = ".webm";
 const SegPlaceholder = "https://"+location.host+"/static/loading" + SegExt;
+const SegBitrate = 20 * 1000;
 
 module.exports = {
   SegLen: SegmentLength,
   RefreshInterval: RefreshInterval,
   SegMime: SegMime,
   SegExt: SegExt,
-  SegPlaceholder: SegPlaceholder
+  SegPlaceholder: SegPlaceholder,
+  SegBitrate: SegBitrate
 };
