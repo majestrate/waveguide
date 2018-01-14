2c93f2cbf3a92a396e93e9f91617097611ccaf06
Jeff <ampernand@gmail.com>
2018-01-14 13:57:03 -0500

    add media source abstraction layer

--- js/cam.js
+++ js/cam.js
@@ -1,2 +1,18 @@
 /** cam.js */
 
+
+var grabcam = function(cb) {
+  var vidopts = {
+    manditory: {
+      chromeMediaSource: 'screen'
+    }
+  };
+  navigator.mediaDevices.getUserMedia({video: vidopts, audio:true}).then(function(src) {
+    cb(null, src);
+  }).catch(function(e) { cb(e, null); });
+};
+
+
+module.exports = {
+  "getCam": grabcam
+}
