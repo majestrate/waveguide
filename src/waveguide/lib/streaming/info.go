b8f3d3b6f6b91f076830995253a912d9403ebaec
Jeff Becker <jeff@i2p.rocks>
2018-02-08 13:34:03 -0500

    make streaming backlog bigger

--- src/waveguide/lib/streaming/info.go
+++ src/waveguide/lib/streaming/info.go
@@ -9,7 +9,7 @@ const StreamUpdateTimeout = time.Minute
 type StreamInfo struct {
 	ID         string    `json:"id"`
 	Username   string    `json:"username"`
-	URLS       [3]string `json:"urls"`
+	URLS       [5]string `json:"urls"`
 	Token      string    `json:"-"`
 	LastUpdate time.Time `json:"-"`
 }
@@ -19,10 +19,12 @@ func (i *StreamInfo) LastTorrent() string {
 }
 
 func (i *StreamInfo) OldestTorrent() string {
-	return i.URLS[2]
+	return i.URLS[4]
 }
 
 func (i *StreamInfo) AddTorrent(url string) {
+	i.URLS[4] = i.URLS[3]
+	i.URLS[3] = i.URLS[2]
 	i.URLS[2] = i.URLS[1]
 	i.URLS[1] = i.URLS[0]
 	i.URLS[0] = url
